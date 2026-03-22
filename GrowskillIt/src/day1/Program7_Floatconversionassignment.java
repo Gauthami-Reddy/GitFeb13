c4a9fefdda9033cd69149e216ac9c7292cc15459
Gauthami <gauthami.gireddy114@gmail.com>
2026-03-22 21:12:12 +0530

    code

---
+++ GrowskillIt/src/day1/Program7_Floatconversionassignment.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class Program7_Floatconversionassignment {
+	public static void main(String[] args) {
+	float value=97.8F;
+	System.out.println(value);	
+	
+	//Convert it into:double (upcasting)
+	double valuefloat=(double)value;
+	System.out.println(valuefloat);	
+	
+	//Convert it into:int(downcasting)
+	int valueint=(int)value;
+	System.out.println(valueint);
+	
+	//Convert the final int value into a character
+	char charValue= (char)value;
+	System.out.println(charValue);
+	
+	
+	
+	
+}
+
+}
---
+++ GrowskillIt/src/day1/Program_DatavariablescodingAssignment.java
@@ -0,0 +1,50 @@
+package day1;
+
+public class Program_DatavariablescodingAssignment {
+	public static void main(String[] args) {
+		//Student age
+		int age=21;
+		System.out.println("Age is" +age);
+		
+		//Student marks
+		float marks=459.9F;
+		System.out.println("marks is" +marks);
+		
+		//Mobile battery percentage (0â€“100)
+		int batterypercentage=79;
+		System.out.println("batterypercentage is" +batterypercentage);
+		
+		//City population
+		
+		long citypopulation=250000;
+		System.out.println("citypopulation is" +citypopulation);
+		
+		//Student grade
+		char grade='A';
+		System.out.println("student grade"  +grade);
+		
+		//Bank balance
+		long bankbalance=500000;
+		System.out.println("bank balance " +bankbalance);
+		
+		//User login status
+		
+		boolean loginstatus= true;
+		System.out.println("login status" +loginstatus);
+		
+		//Distance to college in meters
+		float distance= 1200f;
+		System.out.println("distance to college" +distance );
+		
+		//A very large number:
+		
+		long largenum = 9000000000L;
+		System.out.println("large number" +largenum );
+		
+		
+		
+		
+		
+	}
+
+}
